b4ee4df292a07ff9c8710e0b3892875967350c67
vekonypeter <vekonypeter94@gmail.com>
2020-02-18 10:48:54 +0100

    Add script for removing gone git branches

---
+++ delete-gone-branches.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+branchnames=($(git branch -v | grep '\[gone\]' | awk '{print $1}'))
+count=${#branchnames[@]}
+
+if (( count < 1 )); then
+    echo "No 'gone' branch found"
+    exit 0;
+fi
+
+echo "The following $count branch(es) lost their remote-tracking pair:"
+
+for branchname in "${branchnames[@]}"
+do
+    echo " - $branchname"
+done
+
+read -p "Do you want to delete them? (Y/N) " deleteBranches
+
+if [ "$deleteBranches" == "Y" ] || [ "$deleteBranches" == "y" ]
+then
+    function join_by { local IFS="$1"; shift; echo "$*"; }
+    cmd="git branch -D `join_by " " "${branchnames[@]}"`"
+    echo "Running command: '$cmd'"
+    eval $cmd
+    echo "Done"
+else
+    echo "Skipped"
+fi
